befd682b6c377a27f7143f454f35fb15595d6698
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-04-25 17:15:59 +0900

    ✨ Feat(javabasic): Java 기초 연산자 및 변수 타입과 변환

---
+++ javabasic/src/javabasic/Variable.java
@@ -0,0 +1,25 @@
+package javabasic;
+
+public class Variable {
+
+	public static void main(String[] args) {
+		byte b = 100;
+		short s = 30000;
+		int i = 2100000000;
+		long l = 4381098319084184321L;
+		float f = 100.1f;
+		double d = 1938123213.2432;
+		boolean bool = true;
+		char ch = '가';
+		
+		System.out.println(b);
+		System.out.println(s);
+		System.out.println(i);
+		System.out.println(l);
+		System.out.println(f);
+		System.out.println(d);
+		System.out.println(bool);
+		System.out.println(ch);
+	}
+
+}
