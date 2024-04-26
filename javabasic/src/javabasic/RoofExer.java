b1a607396c6ee8cc3e261ce6aac2a8aad824bde9
Administrator <Administrator@DESKTOP-HUJUVSS>
2024-04-26 17:52:40 +0900

    ✨ Feat(RoofExer): for문을 활용한 별찍기

---
+++ javabasic/src/javabasic/RoofExer.java
@@ -0,0 +1,144 @@
+package javabasic;
+
+public class RoofExer {
+
+	public static void main(String[] args) {
+
+		// 별찍기 1
+		// *
+		// **
+		// ***
+		// ****
+		// *****
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// 별찍기2
+		//     *
+		//    **
+		//   ***
+		//  ****
+		// *****
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 4-i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");				
+			}
+			System.out.println();
+		}
+		System.out.println();
+		
+		
+		// 별찍기3
+		// *****
+		// ****
+		// ***
+		// **
+		// *
+		for (int i = 5; i > 0; i--) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+
+		// 별찍기4
+		// *****  
+		// *   *
+		// *   *
+		// *   *
+		// *****
+		int keynum = 4;
+		for (int i = 0; i < keynum+1; i++) {
+			for (int j = 0; j < keynum+1; j++) {
+				if (i % keynum == 0 || j % keynum == 0) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// 별찍기5
+		// *****  
+		// ** **
+		// * * *
+		// ** **
+		// *****
+		keynum = 4;
+		for (int i = 0; i < keynum+1; i++) {
+			for (int j = 0; j < keynum+1; j++) {
+				if (i % keynum == 0 || j % keynum == 0 || i == j || i + j == keynum) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// 별찍기6 (Z) 
+		// *****
+		//    *
+		//   *
+		//  *
+		//*****
+		keynum = 4;
+		for (int i = 0; i < keynum+1; i++) {
+			for (int j = 0; j < keynum+1; j++) {
+				if (i % keynum == 0 || i + j == keynum) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+		// 별찍기7 (H)
+		// *   *
+		// *   *
+		// *****
+		// *   *
+		// *   *
+		keynum = 4;
+		for (int i = 0; i < keynum+1; i++) {
+			for (int j = 0; j < keynum+1; j++) {
+				if (j % keynum == 0 || i == keynum/2) {
+					System.out.print("*");
+				} else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		System.out.println();
+
+
+		// 별찍기8 (다이아몬드)
+		//  *
+		// ***
+		//*****
+		// ***
+		//  *
+		
+		// 코드 수행
+
+	}
+
+}
